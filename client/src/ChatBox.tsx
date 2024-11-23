17170078a09d76471bfa59b2d2e7ff7931b8a5cc
ajosep06 <ajosep06@students.cs.ubc.ca>
2024-11-22 23:16:18 -0800

    attempting to see if chatbox history works if pinging off my local server vs render - test

--- client/src/ChatBox.tsx
+++ client/src/ChatBox.tsx
@@ -16,7 +16,7 @@ const ChatBox: React.FC = () => {
   useEffect(() => {
     const fetchConversation = async () => {
       try {
-        const response = await fetch('https://mental-health-chatbot-hnn9.onrender.com/history', {
+        const response = await fetch('http://localhost:5002/history', {
           credentials: 'include',
         });
         if (!response.ok) {
@@ -67,7 +67,7 @@ const ChatBox: React.FC = () => {
 
     // Send message to server
     try {
-      const response = await fetch('https://mental-health-chatbot-hnn9.onrender.com/chat', {
+      const response = await fetch('http://localhost:5002/chat', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         credentials: 'include',
